fa538d6b63fe3e73b49dd0d244edb3dbb1db9671
Lawrence Craft <lawrence.craft@gmail.com>
2015-06-15 05:13:41 +0100

    Slight upgrades

--- terrainserver.go
+++ terrainserver.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/negroni"
+	"github.com/jingweno/negroni-gorelic"
 	"github.com/lawrencecraft/terrainmodel/drawer"
 	"github.com/lawrencecraft/terrainmodel/generator"
 	"net/http"
@@ -8,17 +13,31 @@ import (
 )
 
 func main() {
+	port := flag.Int("p", 8822, "Port to listen for connections")
+	nrkey := flag.String("nrkey", "", "NewRelic license key")
+	flag.Parse()
+	log.SetLevel(log.InfoLevel)
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	log.Info("Setting max procs to", runtime.NumCPU())
+
 	g := generator.NewDiamondSquareGenerator(1.0, 1025, 1025)
-	http.HandleFunc("/map", func(w http.ResponseWriter, req *http.Request) {
+
+	r := http.NewServeMux()
+	r.HandleFunc("/map", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "image/png")
 		d := drawer.NewPngDrawer(w)
 		t, _ := g.Generate()
 		d.Draw(t)
 	})
 
-	err := http.ListenAndServe(":8822", nil)
-	if err != nil {
-		panic(err)
+	n := negroni.New()
+
+	if *nrkey != "" {
+		log.Info("Using NewRelic")
+		n.Use(negronigorelic.New(*nrkey, "terrainworker", true))
 	}
+
+	n.UseHandler(r)
+
+	n.Run(fmt.Sprintf(":%d", *port))
 }
